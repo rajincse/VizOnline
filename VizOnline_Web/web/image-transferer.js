669b8312fa7203a84c6a6673daa09a8f1cf3c30a
mershack <meshhome16@gmail.com>
2014-01-26 21:41:12 -0500

    .

--- VizOnline_Web/web/image-transferer.js
+++ VizOnline_Web/web/image-transferer.js
@@ -4,58 +4,39 @@ function ImageTransferer(div, w,h, tch, tcv)
 	this.tch = tch;
 	this.tcv = tcv;
 	this.w = w;
-	this.h = h;
-	this.imageCounter = 29;
+	this.h = h;	
 
 	this.tw = w/tch;
 	this.th = h/tcv;
 
-	this.images = [];
+	this.images = null;
 
-	this.resize = resize;
-
+	
 	this.needsResize = false;
 
 	this.cnt = 0;
-	this.loaded = 0;
+	this.loaded = 0;	
 
-	function resize(w,h)
-	{
-		this.w = w;
-		this.h = h;
-		this.needsResize = true;
-	}
 
 
 	this.imageUpdate = imageUpdate;
         function imageUpdate() {
 
 
-		this.cnt++;
+		if (this.images != null)
+			return;
 
-		if (this.needsResize)
-		{
-			this.tw = this.w/this.tch;
-			this.th = this.h/this.tcv;
-			this.needsResize = false
-			this.imageCounter = 29;
-		}
+		
 
-
-		this.imageCounter = this.imageCounter+1;
-
-		var dif = 1;
-		if (this.imageCounter == 30)
-			dif = 0;
-           
 		this.images = [];
+		this.cnt++;
 		this.loaded = 0;
 		for (var i=0; i<this.tcv; i++)
 		{
 			var rowImages = [];
 			for (var j=0; j<tch; j++)
 			{
-				var url = "VizOnlineServlet?page=viewer&tileX="+j+"&tileY="+i+"&diff="+dif+"&r=" + this.cnt;
+				var url = "VizOnlineServlet?page=viewer&tileX="+j+"&tileY="+i+"&diff=1&r=" + this.cnt;
 				var image = new Image();
 				rowImages.push(image);
 				image.src = url;
@@ -64,7 +45,7 @@ function ImageTransferer(div, w,h, tch, tcv)
 				var imtr = this;
 
 
-                  		  image.onload = function() {
+                  		  image.onload = function() {					
                      		  	imtr.loaded++;
                       		 	imtr.fimloaded();
                    		  };
@@ -76,20 +57,27 @@ function ImageTransferer(div, w,h, tch, tcv)
 
 	this.fimloaded = fimloaded;
 	function fimloaded() {
-console.log(this.loaded + " " + tch*tcv);
+		console.log(this.loaded + " " + tch*tcv);
 		if (this.loaded == tch*tcv)
 		{
-
-			if (this.imageCounter == 30)
+			if (this.images[0][0].width == 1)
 			{
-				this.imageCounter = 0;
+				this.images = null;
+				return;
+			}
+
+			else if (this.images[0][0].width != 500 )
+			{
 				var last;
     				while (last = this.div.lastChild) this.div.removeChild(last);
+				//alert(this.images[0][0].width);
 			}
+
 
 			for (var i=0; i<this.images.length; i++)
 				for (var j=0; j<this.images[i].length; j++)
 				{
+					console.log(this.images[i][j].width);
 					this.div.appendChild(this.images[i][j]);
                         		this.images[i][j].style.width = this.tw + "px";
                         		this.images[i][j].style.height = this.th + "px";
@@ -99,12 +87,12 @@ console.log(this.loaded + " " + tch*tcv);
 				}
 
                        
-			this.images = [];
-console.log("next");
-			this.imageUpdate();
-			
+			this.images = null;			
                     }
                 }
+
+
+	
 
 }
 
