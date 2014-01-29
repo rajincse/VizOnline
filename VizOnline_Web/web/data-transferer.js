188a198837112e07ef4df2abb2820c43878b275c
mershack <meshhome16@gmail.com>
2014-01-28 19:11:44 -0500

    .

--- VizOnline_Web/web/data-transferer.js
+++ VizOnline_Web/web/data-transferer.js
@@ -1,25 +1,52 @@
-var canvas , canvasWidth, canvasHeight;
-function DataTransfer(div,w,h)
+var canvas, canvasWidth, canvasHeight;
+function DataTransfer(div, w, h)
 {
     canvas = div;
     canvasWidth = w;
-    canvasHeight= h;
-   
+    canvasHeight = h;
+
+}
+;
+
+DataTransfer.prototype.readCreatorType = function(callback)
+{
+    var viewerName;
+    var test = function() {//recurring function to ensure the viewer is set
+        viewerName = document.getElementById("viewerName").value;
+        if (viewerName !== "") {
+
+            var url = 'VizOnlineServlet?page=d3viewer&method=readCreatorType';
+            url += "&viewerName=" + document.getElementById("viewerName").value;
+            $.get(url, callback);
+
+            clearInterval(id);
+        }
+    };
+    var id = setInterval(test, 100);
 };
 
-DataTransfer.prototype.readCreatorType = function (callback)
+DataTransfer.prototype.readViewerData = function(callbackMethod)
 {
-    var url = 'VizOnlineServlet?page=d3viewer&method=readCreatorType';
-    $.get(url, callback);
-};
-    
-DataTransfer.prototype.readViewerData = function (callbackMethod)
-{
-    var url = 'VizOnlineServlet?page=d3viewer&method=readViewerData';
-    $.get(url, function(data) {
 
-        callbackMethod(data, canvas,canvasWidth,canvasHeight);
-    });
+    //make sure the viewer name is set
+    var viewerName;
+    var test = function() {//recurring function to ensure the viewer is set
+        viewerName = document.getElementById("viewerName").value;
+        if (viewerName !== "") {
+
+            var url = 'VizOnlineServlet?page=d3viewer&method=readViewerData';
+            url += "&viewerName=" + document.getElementById("viewerName").value;
+
+            $.get(url, function(data) {
+
+                callbackMethod(data, canvas, canvasWidth, canvasHeight);
+            });
+
+
+            clearInterval(id);
+        }
+    };
+    var id = setInterval(test, 100);
 };
 
 
