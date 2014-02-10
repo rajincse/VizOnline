38d49261f7049079e6d29f2a23a1e5f4e41a0b1e
rajincse <rajin.cse@gmail.com>
2014-02-09 23:43:24 -0800

    #27
    BrainViewer and BrainStatsD3Viewer has been added to VizOnline.
    In the BrainStatsD3Viewer the chart is being shown according to selectedTube Property change

---
+++ Perspectives/src/brain/BrainStatsD3ViewerFactory.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brain;
+
+import perspectives.Viewer;
+import perspectives.ViewerFactory;
+
+/**
+ *
+ * @author rajin
+ */
+public class BrainStatsD3ViewerFactory extends ViewerFactory{
+    public static final String CREATOR_TYPE ="BrainStatsD3Viewer";
+    
+
+	@Override
+	public String creatorType() {
+		// TODO Auto-generated method stub
+		return CREATOR_TYPE;
+	}
+
+	@Override
+	public Viewer create(String name) {
+		if (this.isAllDataPresent()) {
+                return new BrainStatsD3Viewer(name, (BrainData)this.getData().get(0));
+            }
+		return null;
+	}
+
+    @Override
+    public ViewerFactory.RequiredData requiredData() {
+        ViewerFactory.RequiredData rd = new ViewerFactory.RequiredData("BrainData","1");
+        return rd;
+    }
+}
