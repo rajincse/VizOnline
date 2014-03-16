5b3bfffec2bab836120e29cee789cb3cb6cd5abb
rajincse <rajin.cse@gmail.com>
2014-03-16 01:47:51 -0400

    #40
    Graph UserStudyGraph implemented. 
    It loads the data and center the view.

--- UserStudyDemo/Desktop/src/userstudy/UserStudyDemoMainClass.java
+++ UserStudyDemo/Desktop/src/userstudy/UserStudyDemoMainClass.java
@@ -1,14 +1,16 @@
 package userstudy;
 
 import perspectives.base.Environment;
+import perspectives.graph.GraphDataFactory;
+import userstudy.graph.GraphUserStudyDataFactory;
+import userstudy.graph.GraphUserStudyViewerFactory;
 
 
 public class UserStudyDemoMainClass {
 	public static void main(String[] args)
 	{
 		Environment e = new Environment(false);
-		
-		e.registerDataSourceFactory(new UserStudyDataFactory());
-		e.registerViewerFactory(new UserStudyViewerFactory());
+		e.registerDataSourceFactory(new GraphUserStudyDataFactory());
+		e.registerViewerFactory(new GraphUserStudyViewerFactory());
 	}
 }
